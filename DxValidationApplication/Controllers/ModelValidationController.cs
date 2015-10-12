f0aeda9f96dcd530165c83770c14f4982585333f
afrancocode <afranco@codeservices.com.mx>
2015-10-12 16:40:46 -0700

    Simple Server Side Model Validation

---
+++ DxValidationApplication/Controllers/ModelValidationController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using DxValidationApplication.Models.ModelValidation;
+
+namespace DxValidationApplication.Controllers
+{
+	public class ModelValidationController : Controller
+	{
+		public ActionResult Play()
+		{
+			return View(new DataViewModel());
+		}
+
+		[HttpPost]
+		public ActionResult DoValidate(DataViewModel model)
+		{
+			if(ModelState.IsValid)
+				return View("Info", model);
+			return View("Play", model);
+		}
+	}
+}
---
+++ DxValidationApplication/Models/ModelValidation/DataViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace DxValidationApplication.Models.ModelValidation
+{
+	public class DataViewModel
+	{
+		[Required(ErrorMessage = "Value is required")]
+		[StringLength(10, ErrorMessage = "Must be under 10 characters")]
+		public string Data { get; set; }
+	}
+}
